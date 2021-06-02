ea3e9f3d9b19acc67059b22060c0a9e24988401c
truonghoangduy <truonghoangduy03111999@gmail.com>
2021-06-03 01:04:18 +0700

    temp code

--- temp.js
+++ temp.js
@@ -1,6 +1,24 @@
+function greet(name, callback) {
+    console.log('Hi' + ' ' + name);
+    callback();
+}
+
+// callback function
+function callMe() {
+    console.log('I am callback function 🔥');
+}
+
+// passing function as an argument
+greet('John', callMe);
+
+
+
 function printMe() {
     console.log("DSC 😁")
 }
 setTimeout(printMe, 3000)
 // Print out DSC after 3s of delay
+
+
+
 
